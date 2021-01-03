8a371db3256510294621996b008dca6da769cc01
seisen <jochim6.2@hotmail.fr>
2021-01-03 18:04:38 +0100

    NormalMod created & hslToRgb

--- src/App.js
+++ src/App.js
@@ -1,91 +1,15 @@
-import ColorPicker from '@radial-color-picker/react-color-picker';
-import '@radial-color-picker/react-color-picker/dist/react-color-picker.min.css';
+
 import './App.css';
 import * as React from "react";
 import { Form } from 'react-bootstrap';
+import {NormalMode} from "./NormalMod";
 
 function App() {
-  const [color, setColor] = React.useState({
-    hue: 90,
-    saturation: 100,
-    luminosity: 50,
-    alpha: 1,
-  });
-  const onInput = hue => {
-    setColor(prev => {
-      return {
-        ...prev,
-        hue,
-      };
-    });
-  };
-  const changeSaturation = (event) => {setColor
-    (prevState => {
-        return{
-            ...prevState,
-            saturation: event.target.value,
-        };
-    });
-  };
-    const changeLuminosity = (event) => {setColor
-    (prevState => {
-        return{
-            ...prevState,
-            luminosity: event.target.value,
-        };
-    });
-    };
-
-  return (
-      <>
-        <ColorPicker {...color} onInput={onInput} />
-
-        <div>
-            <input type="range" min='0' max='100' id='saturationInput'
-                   onChange={changeSaturation}
-                   style={{backgroundImage: 'linear-gradient(to right,'
-                           +hslToRgb(color.hue,0,color.luminosity)+
-                           ','+hslToRgb(color.hue,50,color.luminosity)+
-                            ','+hslToRgb(color.hue,100,color.luminosity)+')'}}
-            />
-
-            <input type="range" min='0' max='100' id='luminosityInput'
-                   onChange={changeLuminosity}
-                   style={{backgroundImage: 'linear-gradient(to right,'
-                           +hslToRgb(color.hue,color.saturation,0)+
-                           ','+hslToRgb(color.hue,color.saturation,50)+
-                           ','+hslToRgb(color.hue,color.saturation,100)+')'}}/>
-        </div>
-
-
-
-      </>
-  );
+    return(
+        <>
+            <NormalMode/>
+        </>
+    );
 }
-function hslToRgb(h, s, l){
-    let r, g, b;
-    h=h/360;
-    s=s/100;
-    l=l/100;
 
-    if(s == 0){
-        r = g = b = l; // achromatic
-    }else{
-        let hue2rgb = function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
-            return p;
-        }
-
-        let q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        let p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
-    }
-    return 'rgba('+r*255+','+g*255+','+b*255+',1)'
-}
 export default App;
---
+++ src/NormalMod.js
@@ -0,0 +1,88 @@
+import randomColor from "randomcolor";
+
+import * as React from "react";
+import ColorPicker from "@radial-color-picker/react-color-picker";
+import {hslToRgb} from "./hslToRgbf";
+
+import '@radial-color-picker/react-color-picker/dist/react-color-picker.min.css';
+
+
+export function NormalMode(){
+    const [color, setColor] = React.useState({
+        hue: 90,
+        saturation: 100,
+        luminosity: 50,
+        alpha: 1,
+    });
+    const [backcolor, setBackcolor] = React.useState({
+        left:randomColor(),
+        right:randomColor()
+    })
+
+    const onInput = hue => {
+        setColor(prev => {
+            return {
+                ...prev,
+                hue,
+            };
+        });
+    };
+    const changeSaturation = (event) => {setColor
+    (prevState => {
+        return{
+            ...prevState,
+            saturation: event.target.value,
+        };
+    });
+    };
+    const changeLuminosity = (event) => {setColor
+    (prevState => {
+        return{
+            ...prevState,
+            luminosity: event.target.value,
+        };
+    });
+    };
+
+    return (
+        <>
+
+            <div id='back'>
+                <div style={{
+                    width:'50%',
+                    height:'100%',
+                    backgroundColor: backcolor.left
+                }}/>
+
+                <div style={{
+                    width:'50%',
+                    height:'100%',
+                    backgroundColor: backcolor.right
+
+                }}/>
+            </div>
+
+            <div id='color-picker'>
+                <ColorPicker {...color} onInput={onInput} id='cp' />
+
+                <div id='inputs'>
+                    <input type="range" min='0' max='100' id='saturationInput'
+                           onChange={changeSaturation}
+                           style={{backgroundImage: 'linear-gradient(to right,'
+                                   +hslToRgb(color.hue,0,color.luminosity)+
+                                   ','+hslToRgb(color.hue,50,color.luminosity)+
+                                   ','+hslToRgb(color.hue,100,color.luminosity)+')'}}
+                    />
+
+                    <input type="range" min='0' max='100' id='luminosityInput'
+                           onChange={changeLuminosity}
+                           style={{backgroundImage: 'linear-gradient(to right,'
+                                   +hslToRgb(color.hue,color.saturation,0)+
+                                   ','+hslToRgb(color.hue,color.saturation,50)+
+                                   ','+hslToRgb(color.hue,color.saturation,100)+')'}}/>
+                </div>
+            </div>
+
+        </>
+    );
+}
