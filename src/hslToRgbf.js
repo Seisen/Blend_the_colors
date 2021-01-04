8c0dec996c1aeb261ac6506c808b72f68b912efe
seisen <jochim6.2@hotmail.fr>
2021-01-04 15:57:35 +0100

    deleted hsltorgb & used delaE func to get the score

--- src/hslToRgbf.js
+++ src/hslToRgbf.js
@@ -24,28 +24,3 @@ export function hslToRgb(h, s, l){
     }
     return 'rgba('+r*255+','+g*255+','+b*255+',1)'}
 
-export function rgbToHsv(r, g, b) {
-    r /= 255;
-    g /= 255;
-    b /= 255;
-
-    let max = Math.max(r, g, b), min = Math.min(r, g, b);
-    let h, s, v = max;
-
-    let d = max - min;
-    s = max == 0 ? 0 : d / max;
-
-    if (max == min) {
-        h = 0; // achromatic
-    } else {
-        switch (max) {
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-
-        h /= 6;
-    }
-
-    return [ h, s, v ];
-}
